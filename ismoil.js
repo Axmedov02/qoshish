11681c51529fe05fd941a73f8e5605f4d67f3f46
ismoil <FaxriddinAbdumajidov31072003>
2023-06-01 23:46:09 +0500

    bir

---
+++ ismoil.js
@@ -0,0 +1,42 @@
+var name = +prompt('  4 + 4 = qoshuv amalni bajaring')
+
+if(name == 8){
+  alert("tog'ri bajardingiz");
+}else if (isNaN(name)){
+    alert('tori yozin aka')
+}else{
+  alert("javob notog'ri qayta urunib koring");
+}
+
+
+var name = +prompt('  4 - 4 = ayruv amalni bajaring')
+
+if(name == 0){
+  alert("tog'ri bajardingiz");
+}else if (isNaN(name)){
+    alert('tori yozin aka')
+}else{
+  alert("javob notog'ri qayta urunib koring");
+}
+
+
+var name = +prompt('  4 * 4 = kopaytruv amalni bajaring')
+
+if(name == 16){
+  alert("tog'ri bajardingiz");
+}else if (isNaN(name)){
+    alert('tori yozin aka')
+}else{
+  alert("javob notog'ri qayta urunib koring");
+}
+
+
+var name = +prompt('  4 / 4 = boluv amalni bajaring')
+
+if(name == 1){
+  alert("tog'ri bajardingiz");
+}else if (isNaN(name)){
+    alert('tori yozin aka')
+}else{
+  alert("javob notog'ri qayta urunib koring");
+}
